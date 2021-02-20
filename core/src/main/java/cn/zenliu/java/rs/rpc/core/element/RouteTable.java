31e0326c8dd8d460573577e3db222a0c7b92c14c
Zen.Liu <lcz20@163.com>
2021-02-21 00:17:55 +0800

    progress on RouteTable

--- core/src/main/java/cn/zenliu/java/rs/rpc/core/element/RouteTable.java
+++ core/src/main/java/cn/zenliu/java/rs/rpc/core/element/RouteTable.java
@@ -35,24 +35,21 @@ public interface RouteTable {
 
     void addMethod(String address, ServiceMethod method);
 
-    final class WeightedRemote {
+    final class RouteRemote {
         final Remote remote;
-        final Integer jump;
+        final Route route;
 
-        WeightedRemote(Remote remote, Integer jump) {
+        RouteRemote(Remote remote, Route route) {
             this.remote = remote;
-            this.jump = jump;
+            this.route = route;
         }
 
-        WeightedRemote updateWeight(Integer jump) {
-            return new WeightedRemote(remote, jump);
-        }
 
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
-            if (!(o instanceof WeightedRemote)) return false;
-            WeightedRemote that = (WeightedRemote) o;
+            if (!(o instanceof RouteRemote)) return false;
+            RouteRemote that = (RouteRemote) o;
             return remote.equals(that.remote);
         }
 
@@ -63,7 +60,7 @@ public interface RouteTable {
     }
 
     final class RouteTableImpl implements RouteTable {
-        final Map<String, ConcurrentSkipListSet<WeightedRemote>> registry = new ConcurrentHashMap<>();
+        final Map<String, ConcurrentSkipListSet<RouteRemote>> registry = new ConcurrentHashMap<>();
         final Map<String, ServiceMethod> methods = new ConcurrentHashMap<>();
         final Map<String, ServiceStreamMethod> streams = new ConcurrentHashMap<>();
         final Cache<Class<?>, Object> services = Cache.build(null, false);
@@ -72,7 +69,7 @@ public interface RouteTable {
         private RouteTableImpl(Logger log) {
             this.log = log;
             RemoteObserverUtil.observer.asFlux().subscribe(e -> {
-                final List<Tuple2<WeightedRemote, ConcurrentSkipListSet<WeightedRemote>>> remotes = findRemote(e.v2);
+                final List<Tuple2<RouteRemote, ConcurrentSkipListSet<RouteRemote>>> remotes = findRemote(e.v2);
                 if (remotes.isEmpty()) return;
                 switch (e.v1) {
                     case WEIGHT_CHANGE:
@@ -88,12 +85,12 @@ public interface RouteTable {
             });
         }
 
-        private List<Tuple2<WeightedRemote, ConcurrentSkipListSet<WeightedRemote>>> findRemote(Remote remote) {
-            List<Tuple2<WeightedRemote, ConcurrentSkipListSet<WeightedRemote>>> result = new ArrayList<>();
-            for (ConcurrentSkipListSet<WeightedRemote> value : registry.values()) {
-                for (WeightedRemote weightedRemote : value) {
-                    if (weightedRemote.remote.equals(remote)) {
-                        result.add(Tuple.tuple(weightedRemote, value));
+        private List<Tuple2<RouteRemote, ConcurrentSkipListSet<RouteRemote>>> findRemote(Remote remote) {
+            List<Tuple2<RouteRemote, ConcurrentSkipListSet<RouteRemote>>> result = new ArrayList<>();
+            for (ConcurrentSkipListSet<RouteRemote> value : registry.values()) {
+                for (RouteRemote routeRemote : value) {
+                    if (routeRemote.remote.equals(remote)) {
+                        result.add(Tuple.tuple(routeRemote, value));
                     }
                 }
             }
@@ -128,13 +125,13 @@ public interface RouteTable {
             for (String s : meta.getRoutes()) {
                 Route r = Route.fromRoute(s);
                 final String address = r.getAddress();
-                ConcurrentSkipListSet<WeightedRemote> lists = registry.get(address);
-                final WeightedRemote weightedRemote = new WeightedRemote(remote, r.jumps());
+                ConcurrentSkipListSet<RouteRemote> lists = registry.get(address);
+                final RouteRemote routeRemote = new RouteRemote(remote, r);
                 if (lists == null)
-                    lists = new ConcurrentSkipListSet<>(Comparator.comparingInt(x -> x.remote.getIndex() + x.jump));
+                    lists = new ConcurrentSkipListSet<>(Comparator.comparingInt(x -> x.remote.getIndex() + x.route.jumps()));
                 else
-                    lists.remove(weightedRemote);
-                lists.add(weightedRemote);
+                    lists.remove(routeRemote);
+                lists.add(routeRemote);
                 registry.put(address, lists);
             }
             return this;
